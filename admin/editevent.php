444d8a6c3a906e1412a7900648894a5d542359f8
Hamzat <hmztadeleke@gmail.com>
2023-05-11 10:24:09 +0100

    Update

--- admin/editevent.php
+++ admin/editevent.php
@@ -1,51 +1,49 @@
 <?php session_start();
-if(!isset($_SESSION['adminlogged'])){
+if (!isset($_SESSION['adminlogged'])) {
     header("Location:login.php");
 }
 ?>
-<?php 
-include"../includes/head.php";
-include"../includes/dbconnection.php";
-include"../includes/alerts.php";
+<?php
+include "../includes/head.php";
+include "../includes/dbconnection.php";
+include "../includes/alerts.php";
 ?>
 <?php
-                                                                          
-if(isset($_GET['formedit'])){
-    $eventid= $_GET['eventid'];
-    $sql= " SELECT * FROM event WHERE event_id = '$eventid' ";
-    if($result = mysqli_query($conn,$sql)){ 
-        if (mysqli_num_rows($result)>0){
-            
+
+if (isset($_GET['formedit'])) {
+    $eventid = $_GET['eventid'];
+    $sql = " SELECT * FROM event WHERE event_id = '$eventid' ";
+    if ($result = mysqli_query($conn, $sql)) {
+        if (mysqli_num_rows($result) > 0) {
+
             $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-            $event_id= $row['event_id'];
+            $event_id = $row['event_id'];
             $event_name = $row['event_name'];
             $event_venue = $row['event_venue'];
             $event_details = $row['event_details'];
-            $event_date = $row['event_date'];   
+            $event_date = $row['event_date'];
             $event_time = $row['event_time'];
             $attendee = $row['event_attendee'];
-               
         }
-    }else 
-        { 
-        
-            echo "ERROR: Could not able to execute $sql. ".mysqli_error($conn); 
-    } 
+    } else {
 
+        echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+    }
 }
-                   
+
 ?>
 <title> Admin| Dashboard </title>
 </head>
+
 <body>
     <section>
         <div class="container-fluid">
-               <!-- mobile nav -->
-               <?php include "../includes/adminmobile.php";?>
-           
+            <!-- mobile nav -->
+            <?php include "../includes/adminmobile.php"; ?>
+
             <div class="row area">
                 <!-- side nav -->
-                <?php include "../includes/adminside.php";?>
+                <?php include "../includes/adminside.php"; ?>
 
                 <!-- top navigation/header -->
                 <div class="col-md-10">
@@ -55,74 +53,74 @@ if(isset($_GET['formedit'])){
                                 <li class="topnav--link"><i class="fa fa-user mr-2 text-comp"></i><span class="text-comp">Admin</span></li>
                             </ul>
                         </div>
-                    </header>       
+                    </header>
 
                     <div class="row mt-2 mb-2">
-                    </div>             
+                    </div>
 
                     <!-- user info -->
                     <div class="row mt-2">
                         <div class="col-md-12 mb-2 ">
                             <div class="card">
-                                 <div class="card-header bg-dark">
+                                <div class="card-header bg-dark">
                                     <div class="row">
-                                            <div class="col-md-10">
-                                                <h4 class="text-white"><?php echo $event_name;?></h4>
-                                            </div>
-                                            
-                                            <div class="col-md-2"> 
-                                            </div>
-                                    </div>    
+                                        <div class="col-md-10">
+                                            <h4 class="text-white"><?php echo $event_name; ?></h4>
+                                        </div>
+
+                                        <div class="col-md-2">
+                                        </div>
+                                    </div>
                                 </div>
                                 <div class="card-body">
-                                
+
                                     <form action="../process/processevents.php" action="get">
-                                            <input type="hidden" name="eventid"  value="<?php echo $event_id;?>">
-                                        
-                                                <div class="form-group">
-                                                <label for="">Event Name</label>
-                                                    <input type="text" name="name" class="form-control" value="<?php echo $event_name;?>">
-                                                </div>
-                                                <div class="form-group">
-                                                <label for="">Event Description</label>
-                                                    <textarea  name="detail" class="form-control"><?php echo $event_details;?></textarea>
-                                                </div>
-                                                <div class="class="form-group"">
-                                                    <label for="">Eligibility</label>
-                                                    <select name="attendee"  class="form-control" >
-                                                         <option value="<?php echo $attendee;?>"><?php echo $attendee;?></option>
-                                                        <option value="Members Only">Members Only</option>
-                                                        <option value="Open">Open</option>
-                                                    </select>
-                                                </div>
-                                                <div class="form-group">
-                                                <label for="">Event Venue</label>
-                                                    <input type="text" name="venue" class="form-control" value="<?php echo $event_venue;?>">
-                                                </div>
-                                                <div class="form-group">
-                                                <label for="">Event Date</label>
-                                                    <input type="text" name="date" class="form-control" value="<?php echo $event_date;?>">
-                                                </div>
-                                                <div class="form-group">
-                                                <label for="">Event Time</label>
-                                                    <input type="time" name="time" class="form-control" value="<?php echo $event_time;?>">
-                                                </div>
-                                        
+                                        <input type="hidden" name="eventid" value="<?php echo $event_id; ?>">
+
+                                        <div class="form-group">
+                                            <label for="">Event Name</label>
+                                            <input type="text" name="name" class="form-control" value="<?php echo $event_name; ?>">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="">Event Description</label>
+                                            <textarea name="detail" class="form-control"><?php echo $event_details; ?></textarea>
+                                        </div>
+                                        <div class="class=" form-group"">
+                                            <label for="">Eligibility</label>
+                                            <select name="attendee" class="form-control">
+                                                <option value="<?php echo $attendee; ?>"><?php echo $attendee; ?></option>
+                                                <option value="Members Only">Members Only</option>
+                                                <option value="Open">Open</option>
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="">Event Venue</label>
+                                            <input type="text" name="venue" class="form-control" value="<?php echo $event_venue; ?>">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="">Event Date</label>
+                                            <input type="text" name="date" class="form-control" value="<?php echo $event_date; ?>">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="">Event Time</label>
+                                            <input type="time" name="time" class="form-control" value="<?php echo $event_time; ?>">
+                                        </div>
+
                                         <br>
                                         <input type="submit" name="edit" value="Edit" class="btn btn-primary">
                                     </form>
                                 </div>
                             </div>
-                        </div>            
+                        </div>
 
+                    </div>
                 </div>
             </div>
-        </div>
     </section>
 
     <script src="https://cdn.ckeditor.com/4.15.0/standard/ckeditor.js"></script>
     <script>
-        CKEDITOR.replace( 'detail' );
+        CKEDITOR.replace('detail');
     </script>
 
-    <?php include "../includes/footer.php";?>
+    <?php include "../includes/footer.php"; ?>
--- admin/junior_tournament.php
+++ admin/junior_tournament.php
@@ -1,47 +1,50 @@
 <?php session_start();
-if(!isset($_SESSION['adminlogged'])){
+if (!isset($_SESSION['adminlogged'])) {
     header("Location:login.php");
 }
 ?>
-<?php include "../includes/alerts.php";?>
-<?php include "../includes/dbconnection.php";?>
-<?php include "../includes/head.php";?>
-<?php include "generate_junior_tournament_excel.php";?>
+<?php include "../includes/alerts.php"; ?>
+<?php include "../includes/dbconnection.php"; ?>
+<?php include "../includes/head.php"; ?>
+<?php include "generate_junior_tournament_excel.php"; ?>
 <title> Admin| Dashboard </title>
 </head>
+
 <body>
     <section>
         <div class="container-fluid">
-               <!-- mobile nav -->
-               <?php include "../includes/adminmobile.php";?>
-           
+            <!-- mobile nav -->
+            <?php include "../includes/adminmobile.php"; ?>
+
             <div class="row area">
                 <!-- side nav -->
-                <?php include "../includes/adminside.php";?>
+                <?php include "../includes/adminside.php"; ?>
 
                 <!-- top navigation/header -->
                 <div class="col-md-10">
                     <header class="row topnav">
                         <div class="col-md-12">
                             <div>
-                                <?php success_alert();error_alert();?>
+                                <?php success_alert();
+                                error_alert(); ?>
                             </div>
                             <ul class="inline topnav--content">
                                 <li class="topnav--link"><i class="fa fa-user mr-2 text-comp"></i><span class="text-comp">Admin</span></li>
                             </ul>
                         </div>
                     </header>
-    
-                     <!-- table -->
+
+                    <!-- table -->
                     <div class="row mt-3">
                         <div class="col-md-12 mb-2 ">
                             <div class="card">
                                 <div class="card-header bg-dark">
-                                    <h4 class="text-white">Junior Tournament 
-                                    <form action="generate_junior_tournament_excel.php" method="post" style="display: inline; float: right;">			
-                                        <button type="submit" id="dataExport" name="dataExport" value="Export to excel" class="btn btn-info">Export To Excel</button>
-                                    </form></h4>
-                                  </div>
+                                    <h4 class="text-white">Junior Tournament
+                                        <form action="generate_junior_tournament_excel.php" method="post" style="display: inline; float: right;">
+                                            <button type="submit" id="dataExport" name="dataExport" value="Export to excel" class="btn btn-info">Export To Excel</button>
+                                        </form>
+                                    </h4>
+                                </div>
                                 <div class="card-body">
                                     <div class="table-responsive">
                                         <table class="table table-hover table-bordered">
@@ -60,72 +63,73 @@ if(!isset($_SESSION['adminlogged'])){
                                                 <th>Actions</th>
                                             </thead>
                                             <tbody>
-                                            <?php 
+                                                <?php
                                                 date_default_timezone_set('Africa/Lagos');
-                                                $date=date('Y-m-d');
+                                                $date = date('Y-m-d');
 
-                                                $n=1;
-                                                foreach($juniorTournamentsData as $juniorTournament) { ?>
-                                                <tr>
-                                                <td><?php echo $n;?></td>
-                                                <td><?php echo $juniorTournament ['registration_id']; ?></td>
-                                                <td><?php echo $juniorTournament ['email']; ?></td>
-                                                <td><?php echo $juniorTournament ['full_name']; ?></td>
-                                                <td><?php echo $juniorTournament ['gender']; ?></td>  
-                                                <td><?php echo $juniorTournament ['date_of_birth']; ?></td>			   
-                                                <td><?php echo $juniorTournament ['telephone_number']; ?></td>
-                                                <td><?php echo $juniorTournament ['fide']; ?></td>
-                                                <td><?php echo $juniorTournament ['fi_de']; ?></td>			   
-                                                <td><?php echo $juniorTournament ['chess_club']; ?></td>
-                                                <td>
-                                                    <form action="../process/paymentjuniortournament.php" method="Post">
-                                                        <input type='hidden'  name='juniortournamentid' value="<?php echo $juniorTournament ['id']; ?>" />
-                                                        <select name="payment" onchange="this.form.submit()" class="btn">
-                                                            <option value="<?php echo $juniorTournament ['payment']; ?>">
-                                                            <?php echo $juniorTournament ['payment']; ?></option>
-                                                            <option value="Paid">Paid</option>
-                                                            <option value="Unpaid">Unpaid</option>
-                                                        </select>
-                                                    </form>
-                                                </td>
-                                                <td>
-                                                    <!-- Trigger/Open The Modal -->
-                                                    <!-- <button id="myBtn?php echo $juniorTournament ['id']; ?>" class="btn btn-danger">Delete</button> -->
+                                                $n = 1;
+                                                foreach ($juniorTournamentsData as $juniorTournament) { ?>
+                                                    <tr>
+                                                        <td><?php echo $n; ?></td>
+                                                        <td><?php echo $juniorTournament['registration_id']; ?></td>
+                                                        <td><?php echo $juniorTournament['email']; ?></td>
+                                                        <td><?php echo $juniorTournament['full_name']; ?></td>
+                                                        <td><?php echo $juniorTournament['gender']; ?></td>
+                                                        <td><?php echo $juniorTournament['date_of_birth']; ?></td>
+                                                        <td><?php echo $juniorTournament['telephone_number']; ?></td>
+                                                        <td><?php echo $juniorTournament['fide']; ?></td>
+                                                        <td><?php echo $juniorTournament['fi_de']; ?></td>
+                                                        <td><?php echo $juniorTournament['chess_club']; ?></td>
+                                                        <td>
+                                                            <form action="../process/paymentjuniortournament.php" method="Post">
+                                                                <input type='hidden' name='juniortournamentid' value="<?php echo $juniorTournament['id']; ?>" />
+                                                                <select name="payment" onchange="this.form.submit()" class="btn">
+                                                                    <option value="<?php echo $juniorTournament['payment']; ?>">
+                                                                        <?php echo $juniorTournament['payment']; ?></option>
+                                                                    <option value="Paid">Paid</option>
+                                                                    <option value="Unpaid">Unpaid</option>
+                                                                </select>
+                                                            </form>
+                                                        </td>
+                                                        <td>
+                                                            <!-- Trigger/Open The Modal -->
+                                                            <!-- <button id="myBtn?php echo $juniorTournament ['id']; ?>" class="btn btn-danger">Delete</button> -->
 
-                                                        <!-- The Modal -->
-                                                        <!-- <div id="myModal?php echo $juniorTournament ['id']; ?>" class="modal"> -->
+                                                            <!-- The Modal -->
+                                                            <!-- <div id="myModal?php echo $juniorTournament ['id']; ?>" class="modal"> -->
 
-                                                        <!-- Modal content -->
-                                                        <!-- <div class="modal-content"> -->
+                                                            <!-- Modal content -->
+                                                            <!-- <div class="modal-content"> -->
                                                             <!-- <div class="modal-body text-center"> -->
                                                             <!-- <p class="pt-2">Do you really want to delete the user?</p> -->
-                                                                <!-- <hr> -->
-                                                                <!-- <div class="float-right pb-2"> -->
-                                                                    <!-- form for deleting -->
-                                                                    <form action="../process/deletejuniortournament.php" method='post' style="display: inline;">
-                                                                        <input type='hidden'  name='juniortournamentid' value="<?php echo $juniorTournament ['id']; ?>" />
-                                                                        <input type='submit' name='deletejuniortournament' class="btn btn-danger " value="Delete" />
-                                                                    </form>
-                                                                    <!-- <span class="btn btn-success close_window">No</span> -->
-                                                                <!-- </div> -->
+                                                            <!-- <hr> -->
+                                                            <!-- <div class="float-right pb-2"> -->
+                                                            <!-- form for deleting -->
+                                                            <form action="../process/deletejuniortournament.php" method='post' style="display: inline;">
+                                                                <input type='hidden' name='juniortournamentid' value="<?php echo $juniorTournament['id']; ?>" />
+                                                                <input type='submit' name='deletejuniortournament' class="btn btn-danger " value="Delete" />
+                                                            </form>
+                                                            <!-- <span class="btn btn-success close_window">No</span> -->
                                                             <!-- </div> -->
-                                                        <!-- </div> -->
-                                                    <!-- </div> -->
-                                                </td>      
-                                                </tr>
-                                                
-                                                <?php $n++;   } ?>
+                                                            <!-- </div> -->
+                                                            <!-- </div> -->
+                                                            <!-- </div> -->
+                                                        </td>
+                                                    </tr>
+
+                                                <?php $n++;
+                                                } ?>
                                             </tbody>
                                         </table>
                                     </div>
                                 </div>
-                              </div>
+                            </div>
                         </div>
                     </div>
-                    
+
                 </div>
             </div>
         </div>
     </section>
 
-    <?php include "../includes/footer.php";?>
+    <?php include "../includes/footer.php"; ?>
